711f27473d8a426d8444e204b6785062504b2720
Дмитрий Горбунов <bous07@mail.ru>
2023-10-29 16:18:55 +0800

    Delete WeatherForecast, WeatherForecastController, Counter, Fetchdata Change NavMenu.razor Add component ProductList.razor, style ProductList.razor.css Add Model Product (min DTO) Add DataContext EntityFramworkCore Add Migrations Add ProductController

---
+++ FurnitureMarketBlazor/Server/Data/DataContext.cs
@@ -0,0 +1,50 @@
+﻿namespace FurnitureMarketBlazor.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                    new Product
+                    {
+                        Id = 1,
+                        Title = "Отличная кухня 1",
+                        Description = "Описание кухни 1",
+                        ImageUrl = "https://thumb.cloud.mail.ru/weblink/thumb/xw1/GWwc/CUW5BiG5e",
+                        Price = 389999.99m
+                    },
+                    new Product
+                    {
+                        Id = 2,
+                        Title = "Отличная кухня 2",
+                        Description = "Описание кухни 2",
+                        ImageUrl = "https://thumb.cloud.mail.ru/weblink/thumb/xw1/Mxek/CqEtuDEe6",
+                        Price = 189999.99m
+                    },
+                    new Product
+                    {
+                        Id = 3,
+                        Title = "Отличная кухня 3",
+                        Description = "Описание кухни 3",
+                        ImageUrl = "https://thumb.cloud.mail.ru/weblink/thumb/xw1/Xe88/Zj4mZ4pvP",
+                        Price = 289999.99m
+                    },
+                    new Product
+                    {
+                        Id = 4,
+                        Title = "Отличная кухня 4",
+                        Description = "Описание кухни 4",
+                        ImageUrl = "https://thumb.cloud.mail.ru/weblink/thumb/xw1/foWx/771H2QoSZ",
+                        Price = 89999.99m
+                    }
+                );
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ FurnitureMarketBlazor/Server/Migrations/20231028123057_ProductSeeding.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FurnitureMarketBlazor.Server.Migrations
+{
+    public partial class ProductSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "Описание кухни 1", "https://thumb.cloud.mail.ru/weblink/thumb/xw1/GWwc/CUW5BiG5e", 389999.99m, "Отличная кухня 1" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "Описание кухни 2", "https://thumb.cloud.mail.ru/weblink/thumb/xw1/Mxek/CqEtuDEe6", 189999.99m, "Отличная кухня 2" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 3, "Описание кухни 3", "https://thumb.cloud.mail.ru/weblink/thumb/xw1/Xe88/Zj4mZ4pvP", 289999.99m, "Отличная кухня 3" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 4, "Описание кухни 4", "https://thumb.cloud.mail.ru/weblink/thumb/xw1/foWx/771H2QoSZ", 289999.99m, "Отличная кухня 4" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
