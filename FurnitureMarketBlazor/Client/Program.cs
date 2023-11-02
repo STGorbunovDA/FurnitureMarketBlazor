376e3b2f91c439c05c47a1df7cd272a22b9898ca
Дмитрий Горбунов <bous07@mail.ru>
2023-11-03 07:00:28 +0800

    Implementation of visualization on the Client

--- FurnitureMarketBlazor/Client/Program.cs
+++ FurnitureMarketBlazor/Client/Program.cs
@@ -2,16 +2,21 @@ global using FurnitureMarketBlazor.Shared;
 global using System.Net.Http.Json;
 global using FurnitureMarketBlazor.Client.Services.ProductService;
 global using FurnitureMarketBlazor.Client.Services.CategoryService;
+global using FurnitureMarketBlazor.Client.Services.CartService;
 using FurnitureMarketBlazor.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductServiceClient, ProductServiceClient>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICategoryServiceClient, CategoryServiceClient>();
+builder.Services.AddScoped<ICartServiceClient, CartServiceClient>();
+
 
 await builder.Build().RunAsync();
