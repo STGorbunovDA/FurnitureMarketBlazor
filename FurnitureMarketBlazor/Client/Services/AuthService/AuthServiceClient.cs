ee8178a465a2b172e89baa2fee41f5a062e14279
Дмитрий Горбунов <bous07@mail.ru>
2023-11-05 17:19:24 +0800

    Implementation of the password change method on the client (User Profile Page & Authentication Middleware.)

--- FurnitureMarketBlazor/Client/Services/AuthService/AuthServiceClient.cs
+++ FurnitureMarketBlazor/Client/Services/AuthService/AuthServiceClient.cs
@@ -6,6 +6,12 @@
 
         public AuthServiceClient(HttpClient http) => _http = http;
 
+        public async Task<ServiceResponse<bool>> ChangePassword(UserChangePassword request)
+        {
+            var result = await _http.PostAsJsonAsync("api/auth/change-password", request.Password);
+            return await result.Content.ReadFromJsonAsync<ServiceResponse<bool>>();
+        }
+
         public async Task<ServiceResponse<string>> Login(UserLogin request)
         {
             var result = await _http.PostAsJsonAsync("api/auth/login", request);
