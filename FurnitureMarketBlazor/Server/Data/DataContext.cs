39b4a26e377457a0990ad6b0740c72b4eea93970
Дмитрий Горбунов <bous07@mail.ru>
2023-11-06 22:56:42 +0800

    Creating Order and OrderItem. Migration.

--- FurnitureMarketBlazor/Server/Data/DataContext.cs
+++ FurnitureMarketBlazor/Server/Data/DataContext.cs
@@ -8,11 +8,16 @@
         public DbSet<ProductVariant> ProductVariants { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<OrderItem>()
+                .HasKey(oi => new { oi.OrderId, oi.ProductId, oi.ProductTypeId });
+
             modelBuilder.Entity<CartItem>()
                .HasKey(ci => new { ci.UserId, ci.ProductId, ci.ProductTypeId });
 
